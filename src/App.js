9418be7aa01b1bb19a844778d40ba47d2657601e
Carlos <carlos@falconmasters.com>
2021-02-26 17:48:08 -0600

    Agregando archivos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Slideshow, Slide, TextoSlide} from './componentes/Slideshow'
+import './estilos.css';
+import styled from 'styled-components';
+import img1 from './img/1.jpg';
+import img2 from './img/2.jpg';
+import img3 from './img/3.jpg';
+import img4 from './img/4.jpg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	return (
+		<main>
+			<Titulo>Productos Destacados</Titulo>
+			<Slideshow controles={true}>
+				<Slide>
+					<a href="https://www.falconmaters.com">
+						<img src={img1} alt=""/>
+					</a>
+					<TextoSlide>
+						<p>15% descuento en productos Apple</p>
+					</TextoSlide>
+				</Slide>
+				<Slide>
+					<a href="https://www.falconmaters.com">
+						<img src={img2} alt=""/>
+					</a>
+					<TextoSlide>
+						<p>15% descuento en productos Apple</p>
+					</TextoSlide>
+				</Slide>
+				<Slide>
+					<a href="https://www.falconmaters.com">
+						<img src={img3} alt=""/>
+					</a>
+					<TextoSlide>
+						<p>15% descuento en productos Apple</p>
+					</TextoSlide>
+				</Slide>
+				<Slide>
+					<a href="https://www.falconmaters.com">
+						<img src={img4} alt=""/>
+					</a>
+					<TextoSlide>
+						<p>15% descuento en productos Apple</p>
+					</TextoSlide>
+				</Slide>
+			</Slideshow>
+
+			<Titulo>Productos Destacados</Titulo>
+			<Slideshow controles={true} autoplay={true} velocidad="3000" intervalo="5000">
+				<Slide>
+					<a href="https://www.falconmaters.com">
+						<img src={img1} alt=""/>
+					</a>
+					<TextoSlide colorFondo="navy">
+						<p>15% descuento en productos Apple</p>
+					</TextoSlide>
+				</Slide>
+				<Slide>
+					<a href="https://www.falconmaters.com">
+						<img src={img2} alt=""/>
+					</a>
+					<TextoSlide>
+						<p>15% descuento en productos Apple</p>
+					</TextoSlide>
+				</Slide>
+			</Slideshow>
+		</main>
+	);
 }
 
+const Titulo = styled.p`
+	font-size: 18px;
+	font-weight: 700;
+	text-transform: uppercase;
+	margin-bottom: 10px;
+`;
+ 
 export default App;
---
+++ src/componentes/Slideshow.js
@@ -0,0 +1,177 @@
+import React, {useRef, useEffect, useCallback} from 'react';
+import {ReactComponent as FlechaIzquierda} from './../img/iconmonstr-angel-left-thin.svg';
+import {ReactComponent as FlechaDerecha} from './../img/iconmonstr-angel-right-thin.svg';
+import styled from 'styled-components';
+
+const Slideshow = ({
+		children,
+		controles = false,
+		autoplay = false,
+		velocidad="500",
+		intervalo="5000"
+	}) => {
+	const slideshow = useRef(null);
+	const intervaloSlideshow = useRef(null);
+
+	const siguiente = useCallback(() => {
+		// Comprobamos que el slideshow tenga elementos
+		if(slideshow.current.children.length > 0){
+			console.log('Siguiente')
+
+			// Obtenemos el primer elemento del slideshow.
+			const primerElemento = slideshow.current.children[0];
+
+			// Establecemos la transicion para el slideshow.
+			slideshow.current.style.transition = `${velocidad}ms ease-out all`;
+
+			const tamañoSlide = slideshow.current.children[0].offsetWidth;
+
+			// Movemos el slideshow
+			slideshow.current.style.transform = `translateX(-${tamañoSlide}px)`;
+
+			const transicion = () => {
+				// Reiniciamos la posicion del Slideshow.
+				slideshow.current.style.transition = 'none';
+				slideshow.current.style.transform = `translateX(0)`;
+
+				// Tomamos el primer elemento y lo mandamos al final.
+				slideshow.current.appendChild(primerElemento);
+
+				slideshow.current.removeEventListener('transitionend', transicion);
+			}
+
+			// Eventlistener para cuando termina la animacion.
+			slideshow.current.addEventListener('transitionend', transicion);
+
+		}
+	}, [velocidad]);
+	
+	const anterior = () => {
+		console.log('Anterior');
+		if(slideshow.current.children.length > 0){
+			// Obtenemos el ultimo elemento del slideshow.
+			const index = slideshow.current.children.length - 1;
+			const ultimoElemento = slideshow.current.children[index];
+			slideshow.current.insertBefore(ultimoElemento, slideshow.current.firstChild);
+			
+			slideshow.current.style.transition = 'none';
+			const tamañoSlide = slideshow.current.children[0].offsetWidth;
+			slideshow.current.style.transform = `translateX(-${tamañoSlide}px)`;
+		
+			setTimeout(() => {
+				slideshow.current.style.transition = `${velocidad}ms ease-out all`;
+				slideshow.current.style.transform = `translateX(0)`;
+			}, 30);
+		}
+	}
+
+	useEffect(() => {
+		if(autoplay){
+			intervaloSlideshow.current = setInterval(() => {
+				siguiente();
+			}, intervalo);
+	
+			// Eliminamos los intervalos
+			slideshow.current.addEventListener('mouseenter', () => {
+				clearInterval(intervaloSlideshow.current);
+			});
+	
+			// Volvemos a poner el intervalo cuando saquen el cursor del slideshow
+			slideshow.current.addEventListener('mouseleave', () => {
+				intervaloSlideshow.current = setInterval(() => {
+					siguiente();
+				}, intervalo);
+			});
+		}
+	}, [autoplay, intervalo, siguiente]);
+
+	return (
+		<ContenedorPrincipal>
+			<ContenedorSlideshow ref={slideshow}>
+				{children}
+			</ContenedorSlideshow>
+			{controles && <Controles>
+				<Boton onClick={anterior}>
+					<FlechaIzquierda />
+				</Boton>
+				<Boton derecho onClick={siguiente}>
+					<FlechaDerecha />
+				</Boton>
+			</Controles>}
+		</ContenedorPrincipal>
+	);
+}
+
+const ContenedorPrincipal = styled.div`
+	position: relative;
+`;
+
+const ContenedorSlideshow = styled.div`
+	display: flex;
+	flex-wrap: nowrap;
+`;
+
+const Slide = styled.div`
+	min-width: 100%;
+	overflow: hidden;
+	transition: .3s ease all;
+	z-index: 10;
+	/* max-height: 500px; */
+	position: relative;
+
+	img {
+		width: 100%;
+		vertical-align: top;
+	}
+`;
+
+const TextoSlide = styled.div`
+	background: ${props => props.colorFondo ? props.colorFondo : 'rgba(0,0,0,.3)'};
+	color: ${props => props.colorTexto ? props.colorTexto : '#fff'};
+	width: 100%;
+	padding: 10px 60px;
+	text-align: center;
+	position: absolute;
+	bottom: 0;
+
+	@media screen and (max-width: 700px) {
+		position: relative;
+		background: #000;
+	}
+`;
+
+const Controles = styled.div`
+	position: absolute;
+	top: 0;
+	z-index: 20;
+	width: 100%;
+	height: 100%;
+	pointer-events: none;
+`;
+
+const Boton = styled.button`
+	pointer-events: all;
+	background: none;
+	border: none;
+	cursor: pointer;
+	outline: none;
+	width: 50px;
+	height: 100%;
+	text-align: center;
+	position: absolute;
+	transition: .3s ease all;
+	/* &:hover {
+		background: rgba(0,0,0,.2);
+		path {
+			fill: #fff;
+		}
+	} */
+
+	path {
+		filter: ${props => props.derecho ? 'drop-shadow(-2px 0px 0px #fff)' : 'drop-shadow(2px 0px 0px #fff)'};
+	}
+
+	${props => props.derecho ? 'right: 0' : 'left: 0'}
+`;
+ 
+export {Slideshow, Slide, TextoSlide};
